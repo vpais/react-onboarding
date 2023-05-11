1b1276d31d858202f1439913f24f58dbd32235fe
vpais <v.pais@runtime-revolution.com>
2023-05-11 12:51:51 +0100

    :fire: Delete unnecessary imports

--- src/components/tvshow.jsx
+++ src/components/tvshow.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { getTvshows } from "../tvshows";
-import { useLoaderData } from "react-router-dom";
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
