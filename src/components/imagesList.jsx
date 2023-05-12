15af57a4ae628d5e486f19e9b2a63c64c7ee78f9
vpais <v.pais@runtime-revolution.com>
2023-05-12 16:22:19 +0100

    :pencil2: put import in const

--- src/components/imagesList.jsx
+++ src/components/imagesList.jsx
@@ -3,19 +3,20 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 export default function ImagesList({ data }) {
-
+  const imgPath = import.meta.env.VITE_API_IMG_PREFIX;
+  
   return (
     <ImageList cols={6} gap={50}>
       {data.map((item, index) => (
         <div key={item.id}>
           <ImageListItem>
             <img
-              src={import.meta.env.VITE_API_IMG_PREFIX + item.poster_path}
+              src={imgPath + item.poster_path}
               alt={item.name}
               loading="lazy"
             />
             <ImageListItemBar
-              title={`${index+1}. ${item.name}`}
+              title={`${index + 1}. ${item.name}`}
               subtitle={item.overview}
             />
           </ImageListItem>
