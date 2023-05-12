31d8592b569fadf9997801017bcd52b4c6a4e5e8
vpais <v.pais@runtime-revolution.com>
2023-05-12 16:14:18 +0100

    :pencil2: change key of list, restructure src -img

--- src/components/imagesList.jsx
+++ src/components/imagesList.jsx
@@ -2,15 +2,20 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 
-export default function imagesList({data}) {
+export default function ImagesList({ data }) {
+  
+  function getSrcValue(item){
+    return import.meta.env.VITE_API_IMG_PREFIX + item.poster_path
+  }
+
   return (
     <ImageList cols={6} gap={50}>
       {data.map((item, index) => (
-        <div key={index}>
+        <div key={item.id}>
           <ImageListItem>
             <img
-              src={import.meta.env.VITE_API_IMG_PREFIX + item.poster_path}
-              srcSet={import.meta.env.VITE_API_IMG_PREFIX + item.poster_path}
+              src={getSrcValue(item)}
+              srcSet={getSrcValue(item)}
               alt={item.name}
               loading="lazy"
             />
